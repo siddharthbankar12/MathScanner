cee33588267f3a3facacbc1d6b4df95fc6b90be8
Siddharth Bankar <siddharthbankar1204@gmail.com>
2024-06-21 00:51:02 +0530

    changes

---
+++ app/src/main/java/com/example/solveit/MainActivity.java
@@ -0,0 +1,207 @@
+package com.example.solveit;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    SurfaceView cameraView;
+    TextView textView;
+    CameraSource cameraSource;
+    ImageButton swapButton;
+    EditText expressionEditText;
+    RelativeLayout layout;
+
+    private static final int requestPermissionID = 101;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        cameraView = findViewById(R.id.surfaceView);
+        textView = findViewById(R.id.textView);
+        swapButton = findViewById(R.id.swapButton);
+        expressionEditText = findViewById(R.id.expressionEditText);
+        layout = findViewById(R.id.relativeLayout);
+
+        swapButton.setOnClickListener(view -> {
+            if (cameraView.getVisibility() == View.VISIBLE) {
+                cameraSource.stop();
+                cameraView.setVisibility(View.GONE);
+                layout.setVisibility(View.VISIBLE);
+            } else {
+                layout.setVisibility(View.GONE);
+                cameraView.setVisibility(View.VISIBLE);
+                try {
+                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                        // TODO: Consider calling
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
+                        return;
+                    }
+                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                        // TODO: Consider calling
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
+                        return;
+                    }
+                    cameraSource.start(cameraView.getHolder());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            textView.setText("");
+        });
+
+        expressionEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                try {
+                    textView.setText(new MathEvaluation(s.toString().replace("\n", "")).parse());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+
+        startCameraSource();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == requestPermissionID && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            try {
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                cameraSource.start(cameraView.getHolder());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Toast.makeText(this, "Permission not granted", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void startCameraSource() {
+
+        final TextRecognizer textRecognizer = new TextRecognizer.Builder(getApplicationContext()).build();
+
+        if (textRecognizer.isOperational()) {
+            cameraSource = new CameraSource.Builder(getApplicationContext(), textRecognizer)
+                    .setFacing(CameraSource.CAMERA_FACING_BACK)
+                    .setRequestedPreviewSize(1280, 1024)
+                    .setAutoFocusEnabled(true)
+                    .setRequestedFps(4.0f)
+                    .build();
+
+            cameraView.getHolder().addCallback(new SurfaceHolder.Callback() {
+                @Override
+                public void surfaceCreated(SurfaceHolder holder) {
+                    try {
+
+                        if (ActivityCompat.checkSelfPermission(getApplicationContext(),
+                                Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+
+                            ActivityCompat.requestPermissions(MainActivity.this,
+                                    new String[]{Manifest.permission.CAMERA},
+                                    requestPermissionID);
+                        }
+                        cameraSource.start(cameraView.getHolder());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+                }
+
+                @Override
+                public void surfaceDestroyed(SurfaceHolder holder) {
+                    cameraSource.stop();
+                }
+            });
+
+            textRecognizer.setProcessor(new Detector.Processor<TextBlock>() {
+                @Override
+                public void release() {
+                }
+
+                @Override
+                public void receiveDetections(Detector.Detections<TextBlock> detections) {
+                    final SparseArray<TextBlock> items = detections.getDetectedItems();
+                    if (items.size() != 0 ){
+
+                        textView.post(() -> {
+                            try {
+                                textView.setText(new MathEvaluation(items.valueAt(0).getValue()).parse());
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        });
+                    }
+                }
+            });
+        }
+    }
+}
---
+++ app/src/main/java/com/example/solveit/MathEvaluation.java
@@ -0,0 +1,117 @@
+package com.example.solveit;
+
+class MathEvaluation {
+
+    private final String expression;
+    private int ch, pos = -1;
+    private boolean wrongResult = false;
+
+    MathEvaluation(String expression){
+        this.expression = expression;
+    }
+
+    String parse() {
+        nextChar();
+        double x = parseLowArithmetic();
+        if (pos < expression.length())
+            wrongResult = true;
+        if (!wrongResult)
+            return String.valueOf(x);
+        else
+            return "Wrong expression";
+    }
+
+    private double parseLowArithmetic() {
+        double x = parseMediumArithmetic();
+        for (;;) {
+            if (evaluate('+'))
+                // addition
+                x += parseMediumArithmetic();
+            else if (evaluate('-'))
+                // subtraction
+                x -= parseMediumArithmetic();
+            else
+                return x;
+        }
+    }
+
+    private double parseMediumArithmetic() {
+        double x = parseHighArithmetic();
+        for (;;) {
+            if (evaluate('*'))
+                // multiplication
+                x *= parseHighArithmetic();
+            else if (evaluate('/'))
+                // division
+                x /= parseHighArithmetic();
+            else if (evaluate('%'))
+                // modulus
+                x %= parseHighArithmetic();
+            else
+                return x;
+        }
+    }
+
+    private double parseHighArithmetic() {
+        if (evaluate('-'))
+            // unary minus
+            return -parseHighArithmetic();
+
+        double x = 0;
+        int startPos = this.pos;
+        if (evaluate('(')) {
+            // parentheses
+            x = parseLowArithmetic();
+            evaluate(')');
+        } else if ((ch >= '0' && ch <= '9') || ch == '.') {
+            // numbers
+            while ((ch >= '0' && ch <= '9') || ch == '.')
+                nextChar();
+            x = Double.parseDouble(expression.substring(startPos, this.pos));
+        } else if (ch >= 'a' && ch <= 'z') {
+            // functions
+            while (ch >= 'a' && ch <= 'z') nextChar();
+            String func = expression.substring(startPos, this.pos);
+            x = parseHighArithmetic();
+            switch (func) {
+                case "sqrt":
+                    x = Math.sqrt(x);
+                    break;
+                case "sin":
+                    x = Math.sin(Math.toRadians(x));
+                    break;
+                case "cos":
+                    x = Math.cos(Math.toRadians(x));
+                    break;
+                case "tg":
+                    x = Math.tan(Math.toRadians(x));
+                    break;
+                default:
+                    wrongResult = true;
+                    break;
+            }
+        } else
+            wrongResult = true;
+
+        // ^ looks like A, so will not recognize it!
+        if (evaluate('^'))
+            x = Math.pow(x, parseHighArithmetic());
+
+        return x;
+    }
+
+    // get next char element
+    private void nextChar() {
+        ch = (++pos < expression.length()) ? expression.charAt(pos) : -1;
+    }
+
+    private boolean evaluate(int charToEvaluate) {
+        while (ch == ' ')
+            nextChar();
+        if (ch == charToEvaluate) {
+            nextChar();
+            return true;
+        }
+        return false;
+    }
+}
